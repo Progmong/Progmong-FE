b3b6ba3f020b545fd7ebc1d6cb1bc47c73d53044
Choo ChangWoo <cnckddn0146@gmail.com>
2025-06-25 19:20:05 +0900

    fix(Project): :bug: Eslint 규칙 수정, import시에 @/ 경로 쓸 수 있게 활성화

--- src/components/BaseContainer.jsx
+++ src/components/BaseContainer.jsx
@@ -14,7 +14,7 @@ const SIZE_SET = {
 
 /* 회색 테두리(가장 바깥 프레임) */
 const Frame = styled.div.attrs(({ size = 'md' }) => ({
-  s: SIZE_SET[size] || SIZE_SET.md,
+  $s: SIZE_SET[size] || SIZE_SET.md,
 }))`
   display: block;
   background-color: #ffffff;
@@ -25,7 +25,7 @@ const Frame = styled.div.attrs(({ size = 'md' }) => ({
     0 6px 0 rgba(0, 0, 0, 0.25);
   margin-bottom: 6px; /* 그람자를 보장하기 위한 최소한의 margin */
 
-  font-size: ${({ s }) => s.font};
+  font-size: ${({ $s }) => $s.font};
 `
 
 /* 공통 컴포넌트 */
