512eaeed1cce4c0e3ab29c53493ffc9a15289d31
Heechan Hyun <hyeonhc328@gmail.com>
2025-06-27 11:03:55 +0900

    refactor(mypage): 팻 정보에 따른 다른 이미지 선택을 위한 분기 및 mock data 추가

--- src/pages/MyPage/CharacterStage.jsx
+++ src/pages/MyPage/CharacterStage.jsx
@@ -1,8 +1,16 @@
 import styled from 'styled-components'
 
-import progmongImage from '../../assets/progmong.png'
 import BaseButton from '../../components/BaseButton.jsx'
-import { useModal } from '@/context/ModalContext.jsx' // 예시용
+import { useModal } from '@/context/ModalContext.jsx'
+import pet1_stage1 from '@/assets/pets/pet1_stage1.png'
+import pet1_stage2 from '@/assets/pets/pet1_stage2.png'
+import pet1_stage3 from '@/assets/pets/pet1_stage3.png'
+import pet2_stage1 from '@/assets/pets/pet2_stage1.png'
+import pet2_stage2 from '@/assets/pets/pet2_stage2.png'
+import pet2_stage3 from '@/assets/pets/pet2_stage3.png'
+import pet3_stage1 from '@/assets/pets/pet3_stage1.png'
+import pet3_stage2 from '@/assets/pets/pet3_stage2.png'
+import pet3_stage3 from '@/assets/pets/pet3_stage3.png'
 
 const Stage = styled.section`
   background-color: white;
@@ -43,15 +51,41 @@ const MessageText = styled.div`
   text-overflow: ellipsis;
 `
 
-const CharacterStage = () => {
+const petImagesMap = {
+  1: {
+    1: pet1_stage1,
+    2: pet1_stage2,
+    3: pet1_stage3,
+  },
+  2: {
+    1: pet2_stage1,
+    2: pet2_stage2,
+    3: pet2_stage3,
+  },
+  3: {
+    1: pet3_stage1,
+    2: pet3_stage2,
+    3: pet3_stage3,
+  },
+}
+const mockMessage = ' message'
+const mockPetData = {
+  type: 2, // pet2
+  stage: 3, // stage3
+  name: '에라그몽프로그몽',
+}
+
+const CharacterStage = ({ pet = mockPetData, message = mockMessage }) => {
   const { openModal } = useModal()
+  const petImage = petImagesMap[pet.type]?.[pet.stage]
+
   return (
     <Stage>
       <section>
-        <CharacterImage src={progmongImage} alt="프로그몽" />
+        <CharacterImage src={petImage} alt={`pet${pet.type}_stage${pet.stage}`} />
 
         <MessageBox>
-          <MessageText>애라 모르겠다~!</MessageText>
+          <MessageText>{message}</MessageText>
           <BaseButton onClick={() => openModal('tag-edit', { title: '오늘의 한마디 수정' })}>
             수정하기
           </BaseButton>
