de906eb8e2f6640a8f19a8e8e6d0eb268e8f6cc5
taemin Kim <ruhokim1@naver.com>
2025-06-29 16:48:27 +0900

    fix: 성 이미지 수정 및 위치 조절

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -424,8 +424,8 @@ const Home = () => {
                 $pos={{
                   mobile: { x: 'calc(50% - 170px)', y: 'calc(50% + 40px)' }, // 모바일
                   tablet: { x: 'calc(50% - 320px)', y: 'calc(50% + 150px)' }, // 태블릿
-                  notebook: { x: 'calc(50% - 400px)', y: 'calc(50% + 60px)' }, // 노트북
-                  desktop: { x: 'calc(50% - 500px)', y: 'calc(50% + 120px)' }, // 데스크톱
+                  notebook: { x: 'calc(50% - 400px)', y: 'calc(50% + 40px)' }, // 노트북
+                  desktop: { x: 'calc(50% - 500px)', y: 'calc(50% + 60px)' }, // 데스크톱
                 }}
               >
                 <div style={{ position: 'relative', height: '100%' }}>
