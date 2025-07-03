a7979a3a35eae9db4f98c7b14bfe9684ac1f8edf
Heechan Hyun <hyeonhc328@gmail.com>
2025-07-03 14:40:00 +0900

    feat(mypageContext): 마이페이지 컨텍스트 도입

--- src/context/Modalrenderer.jsx
+++ src/context/Modalrenderer.jsx
@@ -11,6 +11,30 @@ const ModalRenderer = ({ name, props }) => {
       return <TextEditModal {...props} />
     case 'alert':
       return <AlertModal {...props} />
+    case 'nickname-edit':
+      return <TextEditModal {...props} title="닉네임 수정" placeholder="새 닉네임을 입력하세요" />
+    case 'pet-nickname-edit':
+      return <TextEditModal {...props} title="팻 애칭 수정" placeholder="새 애칭을 입력하세요" />
+    case 'password-edit':
+      return (
+        <TextEditModal
+          {...props}
+          title="비밀번호 변경"
+          placeholder="새 비밀번호를 입력하세요"
+          type="password"
+        />
+      )
+    case 'withdraw-confirm':
+      return (
+        <AlertModal
+          {...props}
+          title="회원 탈퇴"
+          message="정말로 회원 탈퇴를 하시겠습니까?"
+          confirmText="탈퇴하기"
+          buttonText="돌아가기"
+        />
+      )
+
     default:
       return null
   }
