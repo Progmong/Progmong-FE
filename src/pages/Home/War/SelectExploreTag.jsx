cd38eb8d550de84db6945d3c596647d418f8c625
taemin3 <xoals8534@gmail.com>
2025-07-08 10:59:52 +0900

    fix: startExplore 함수 추가

--- src/pages/Home/War/SelectExploreTag.jsx
+++ src/pages/Home/War/SelectExploreTag.jsx
@@ -5,6 +5,7 @@ import BaseContainer from '../../../components/BaseContainer'
 import BaseButton from '../../../components/BaseButton'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import useExploreApi from '../../../constants/explore'
 
 const fightBackground = new URL('../../../assets/fight.png', import.meta.url).href
 
@@ -106,7 +107,8 @@ const ExploreTagSelect = () => {
   
 
   const { getUserTags, updateUserTags } = useInterestTagApi()
-
+  const { startExplore } = useExploreApi()
+  
   useEffect(() => {
     getUserTags()
       .then((res) => {
